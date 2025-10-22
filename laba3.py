2befcf41679d789cbe1dd6e69f0e830cb169bf3f
Migda Danil <migda.danil@bk.ru>
2025-10-22 21:35:55 +0300

    commit4

--- laba3.py
+++ laba3.py
@@ -88,20 +88,23 @@ def merge(left, right):
 # Быстрая сортировка
 def quick_sort(arr):
     if len(arr) <= 1:
-        return arr  
+        return arr
     pivot = arr[0]
     left = [x for x in arr[1:] if x < pivot]
     right = [x for x in arr[1:] if x >= pivot]
-    return quick_sort(left) + [pivot] + quick_sort(right)  
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
 
 
 # Тестируем и выводим результат
-d = list(int(input()))
+d = list(map(int, input("Введите числа без пробелов: ")))
+
 print("Пузырьковая сортировка:", bubble_sort(d.copy()))
 print("Сортировка выбором:", selection_sort(d.copy()))
 print("Сортировка вставками:", insertion_sort(d.copy()))
-
+print("Пирамидальная сортировка:", heap_sort(d.copy()))
+print("Сортировка слиянием:", merge_sort(d.copy()))
+print("Быстрая сортировка:", quick_sort(d.copy()))
 
 
 
