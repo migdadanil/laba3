4ecffe94c875a4423d114230a4a50074f776c0c7
Migda Danil <migda.danil@bk.ru>
2025-10-22 21:15:54 +0300

    commit2

--- laba3.py
+++ laba3.py
@@ -33,6 +33,59 @@ def insertion_sort(arr):
     return arr
 
 
+# Пузырьковая сортировка
+def heapify(arr, n, i):
+    largest = i  
+    left = 2 * i + 1  
+    right = 2 * i + 2  
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+
+# Сортировка слиянием
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+
 # Тестируем и выводим результат
 d = list(int(input()))
 print("Пузырьковая сортировка:", bubble_sort(d.copy()))
